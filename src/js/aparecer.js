e792794ecc0fabd9183dcf68a3ee0ea1cad85007
Enne <EnneFofe10@gmail.com>
2023-09-03 10:53:39 -0300

    Responsividade do site
    Deixando o site mais responsivo para as menores telas

---
+++ src/js/aparecer.js
@@ -0,0 +1,5 @@
+// Aplicando a função de aparecer
+window.sr = ScrollReveal({ reset: true });
+
+// Chamando as tags para aparecerem
+sr.reveal('.personagem');
--- src/js/personagens.js
+++ src/js/personagens.js
@@ -1,15 +1,14 @@
-let url = 'https://swapi.dev/api/people/'
-console.log(url)
-
+// Formando a funcionalidade da API
 function mostrarPersonagens() {
+    // Criando a variável para a URL
+    let url = 'https://swapi.dev/api/people/'
     
-    // personagens.innerHTML = ''
-    
+    // Acessando cada info de cada personagem
     for (let i = 1 ; i <= 10 ; i++) {
         fetch(url + i + '/')
         .then(res => { return res.json() } )
         .then(data => {
-            console.log(data)
+            // Adicionando cada info na tela
             let personagem = document.getElementsByClassName('personagem')[i - 1]
             personagem.innerHTML += `<p> <img src="../image/personagem-${i}.jpeg" /> </p>`
             personagem.innerHTML += `Nome: ${data.name} <br>`
@@ -20,8 +19,11 @@ function mostrarPersonagens() {
             personagem.innerHTML += `Ano de nascimento: ${data.birth_year} <br>`
             personagem.innerHTML += `Identidade de gênero: ${data.gender} <br>`
         })
+
+        // Prevendo erros de aplicação
         .catch(error => personagem.innerHTML = '<strong> Erro nos códigos aplicados. </strong>')
     }
-
 }
+
+// Chamando a função para o funcionamento
 mostrarPersonagens()
