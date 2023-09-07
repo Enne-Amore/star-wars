5eca5cc0d423e3ef96d9e6d60b35474985884789
Enne <EnneFofe10@gmail.com>
2023-09-07 17:41:53 -0300

    Cards
    Bons ajustes nos cards

--- src/js/personagens.js
+++ src/js/personagens.js
@@ -9,20 +9,37 @@ function mostrarPersonagens() {
         .then(res => { return res.json() } )
         .then(data => {
             // Adicionando cada info na tela
-            let personagem = document.getElementsByClassName('personagem')[i - 1]
-            personagem.innerHTML += `<p> <img src="../image/personagem-${i}.jpeg" /> </p>`
-            personagem.innerHTML += `Nome: ${data.name} <br>`
-            personagem.innerHTML += `Massa: ${data.mass} <br>`
-            personagem.innerHTML += `Cor do cabelo: ${data.hair_color} <br>`
-            personagem.innerHTML += `Cor da pele: ${data.skin_color} <br>`
-            personagem.innerHTML += `Cor dos olhos: ${data.eye_color} <br>`
-            personagem.innerHTML += `Ano de nascimento: ${data.birth_year} <br>`
-            personagem.innerHTML += `Identidade de gênero: ${data.gender} <br>`
+            let foto = document.getElementsByClassName('foto')[i - 1]
+            foto.innerHTML += `<img src="../image/personagem-${i}.jpeg" />`
         })
+    }
+    for (let n = 1 ; n <= 10 ; n++) {
+            fetch(url + n + '/')
+            .then(res => { return res.json() } )
+            .then(data => {
+                // Adicionando cada info na tela
+                let nome = document.getElementsByClassName('nome')[n - 1]
+                nome.innerHTML += data.name
+        })
+    }
+    for (let a = 1 ; a <= 10 ; a++) {
+            fetch(url + a + '/')
+            .then(res => { return res.json() } )
+            .then(data => {
+                // Adicionando cada info na tela
+                let aspecto = document.getElementsByClassName('aspecto')[a - 1]
+                aspecto.innerHTML += `Massa: ${data.mass} <br>`
+                aspecto.innerHTML += `Cor do cabelo: ${data.hair_color} <br>`
+                aspecto.innerHTML += `Cor da pele: ${data.skin_color} <br>`
+                aspecto.innerHTML += `Cor dos olhos: ${data.eye_color} <br>`
+                aspecto.innerHTML += `Ano de nascimento: ${data.birth_year} <br>`
+                aspecto.innerHTML += `Identidade de gênero: ${data.gender} <br>`
+        })
+    }
 
         // Prevendo erros de aplicação
-        .catch(error => personagem.innerHTML = '<strong> Erro nos códigos aplicados. </strong>')
-    }
+        let erro = document.getElementsByClassName('personagem')[0]
+        .catch(error => erro.innerHTML = '<strong> Erro nos códigos aplicados. </strong>')
 }
 
 // Chamando a função para o funcionamento
