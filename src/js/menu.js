35249810a93a449fffa6dbb0798271a0ef44f063
”Enne” <EnneFofe10@gmail.com>
2023-11-26 18:00:04 -0300

    Acessibilidade
    Ajustando a focagem dos elementos para casar com o passamento do mouse. Além de deixar os nomes das páginas navegáveis pela tecla "tab" apenas se o menu de navegação entre páginas estiver aberto.

--- src/js/menu.js
+++ src/js/menu.js
@@ -8,6 +8,12 @@ menu.addEventListener('click', ()=> {
     if (paginas.style.left = '-80%') {
         paginas.style.left = '0'
         paginas.style.opacity = '100%'
+        
+        // Nomes das páginas navegáveis por tab
+        for (let i = 0 ; i <= 1 ; i++) {
+            let pagina = document.getElementsByClassName('pagina')[i]
+            pagina.style.display = 'flex'
+        }
     }
 })
 
@@ -25,14 +31,16 @@ menu.addEventListener('keypress', (tecla) => {
 
 // Fechar menu
 // Chamando as outras tags com constantes
-let pagina = document.querySelector('.pagina')
 const logo = document.getElementById('logo')
 const nav = document.getElementById('nav')
 const conteudo = document.getElementById('conteudo')
 const rodape = document.getElementById('rodape')
 
-// Adicionando os eventos em cada uma delas
-pagina.addEventListener('click', fecharMenu)
+// Adicionando o evento em cada uma delas
+for (let i = 0 ; i <= 1 ; i++) {
+    let pagina = document.getElementsByClassName('pagina')[i]
+    pagina.addEventListener('click', fecharMenu)
+}
 logo.addEventListener('click', fecharMenu)
 nav.addEventListener('click', fecharMenu)
 conteudo.addEventListener('click', fecharMenu)
@@ -42,4 +50,10 @@ rodape.addEventListener('click', fecharMenu)
 function fecharMenu() {
     paginas.style.left = '-80%'
     paginas.style.opacity = '0'
+
+    // Nomes das páginas não navegáveis por tab
+    for (let i = 0 ; i <= 1 ; i++) {
+        let pagina = document.getElementsByClassName('pagina')[i]
+        pagina.style.display = 'none'
+    }
 }
